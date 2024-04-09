891e79f8db9304eb5fb9b8257e77afbb8871e1d8
jakareya76 <jakareya1306@gmail.com>
2024-04-09 13:02:06 +0600

    create login and signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="container mx-auto">
+      <div className="w-full h-[calc(100vh-100px)] flex flex-col items-center justify-center">
+        <div className="w-full max-w-lg p-8 border rounded-xl">
+          <h2 className="mb-5 text-2xl font-bold">Login Now</h2>
+          <form>
+            <label htmlFor="email">
+              <input
+                type="email"
+                placeholder="email"
+                className="w-full px-4 py-3 my-2 border outline-none"
+              />
+            </label>
+            <label htmlFor="password" className="relative">
+              <input
+                type={`${showPassword ? "text" : "password"}`}
+                placeholder="password"
+                className="w-full px-4 py-3 my-2 border outline-none"
+              />
+              {showPassword ? (
+                <FaRegEye
+                  size={24}
+                  className="absolute top-0 cursor-pointer right-2"
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              ) : (
+                <FaRegEyeSlash
+                  size={25}
+                  className="absolute top-0 cursor-pointer right-2"
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              )}
+            </label>
+            <Link to="/sign-up">New here? Create an account</Link>
+            <button className="w-full my-2 text-xl text-white btn bg-primary hover:bg-blue-800">
+              Login
+            </button>
+          </form>
+          <div className="flex items-center justify-center gap-4 mt-4">
+            <button className="w-[48%] text-lg btn btn-success text-white">
+              Login With Google
+            </button>
+            <button className="w-[48%] text-lg btn btn-warning">
+              Login With Github
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
