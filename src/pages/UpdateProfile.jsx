25ed5b7cb061b081e94ce03375814bde34575d1c
jakareya76 <jakareya1306@gmail.com>
2024-04-13 13:19:04 +0600

    show content on user profiles

--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -1,5 +1,87 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import { toast } from "react-toastify";
+import { updateEmail, updateProfile } from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
+
 const UpdateProfile = () => {
-  return <div>UpdateProfile</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [photo, setPhoto] = useState("");
+
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    setName(user.displayName);
+    setEmail(user.email);
+    setPhoto(user.photoURL);
+  }, []);
+
+  const handleUpdateProfile = async (e) => {
+    e.preventDefault();
+
+    try {
+      await updateProfile(auth.currentUser, {
+        displayName: name,
+        photoURL: photo,
+      });
+
+      await updateEmail(auth.currentUser, email);
+
+      toast.success("Update User Successfully");
+    } catch (error) {
+      if (
+        error.message ===
+        "Firebase: Please verify the new email before changing email. (auth/operation-not-allowed)."
+      ) {
+        toast.success("Please verify the new email before changing email.");
+      } else {
+        toast.success("Something is wrong");
+      }
+    }
+  };
+
+  return (
+    <div className="container mx-auto">
+      <div className="flex items-center justify-center w-full h-[calc(100vh-100px)]">
+        <form onSubmit={handleUpdateProfile} className="w-1/2">
+          <label htmlFor="name">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="w-full my-3 input input-bordered"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label htmlFor="email">
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              className="w-full my-3 input input-bordered"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label htmlFor="PhotoUrl">
+            <input
+              type="text"
+              name="PhotoUrl"
+              placeholder="Photo URL"
+              className="w-full my-3 input input-bordered"
+              value={photo}
+              onChange={(e) => setPhoto(e.target.value)}
+            />
+          </label>
+          <button className="w-full text-xl btn btn-primary">
+            Update Profile
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateProfile;
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,5 +1,32 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="container mx-auto">
+      <div className="w-full h-[calc(100vh-100px)] flex items-center justify-center">
+        <div className="flex flex-col justify-center gap-5 p-5 border rounded-md md:flex-row">
+          <img
+            src={user.photoURL}
+            alt="user"
+            className="md:w-[250px] object-cover"
+          />
+          <div>
+            <p className="p-5 mb-4 font-bold border">Id: {user.uid}</p>
+            <h2 className="font-bold my-3text-xl ">{user.displayName}</h2>
+            <h3 className="my-2 font-medium">{user.email}</h3>
+            <h3 className="my-2 font-medium">{user.photoURL}</h3>
+            <h3 className="my-2 font-medium">
+              <span className="mr-2 font-bold underline">Is Verified :</span>
+              {user.emailVerified.toString()}
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
